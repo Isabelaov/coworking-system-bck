24715d3bfc020bb1af34608b2f84afc5a3c648d0
Isabela Ortega Vasco <isabela2603ov@gmail.com>
2024-06-11 17:06:38 -0500

    cleaned branch

--- src/companies/companies.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CompaniesService } from './companies.service';
-import { CompaniesController } from './companies.controller';
-
-@Module({
-  controllers: [CompaniesController],
-  providers: [CompaniesService],
-})
-export class CompaniesModule {}
